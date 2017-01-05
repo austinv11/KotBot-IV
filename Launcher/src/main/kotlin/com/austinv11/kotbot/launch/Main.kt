c0c0e359aecc0bc03d2f262c61db19bc0f4daa9c
austinv11 <austinv11@gmail.com>
2017-01-05 15:29:42 -0500

    Adds a method of dynamically building the bot for installation

--- Launcher/src/main/kotlin/com/austinv11/kotbot/launch/Main.kt
+++ Launcher/src/main/kotlin/com/austinv11/kotbot/launch/Main.kt
@@ -1,5 +1,57 @@
 package com.austinv11.kotbot.launch
 
+import com.github.kittinunf.fuel.httpGet
+import com.google.gson.Gson
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.system.exitProcess
+
+const val REPO_MIRROR_DIR = "./git_mirror/"
+const val INSTALLATION_DIR = "./bot.jar"
+const val COMMIT_URL = "https://api.github.com/repos/austinv11/KotBot-IV/commits/master"
+const val COMMIT_CACHE = REPO_MIRROR_DIR+"commit.txt"
+const val GIT_REPO = "https://github.com/austinv11/KotBot-IV.git"
+
 fun main(args: Array<String>) {
+    if (args.isEmpty())
+        throw IllegalArgumentException("At least one argument must be provided!")
     
+    val token = args[0]
+    
+    val mirror = File(REPO_MIRROR_DIR)
+    val bot = File(INSTALLATION_DIR)
+    val commit = File(COMMIT_CACHE)
+    
+    val latestCommit = getLatestCommit()
+    if (!mirror.exists() || !commit.exists() || !bot.exists() || commit.readText() != latestCommit) { //Time to (re)install
+        println("Updating the bot...")
+        
+        if (bot.exists())
+            bot.delete()
+        
+        if (mirror.exists())
+            mirror.deleteRecursively()
+        
+        println("Cloning the git repo...")
+        if (ProcessBuilder("git", "clone", GIT_REPO, REPO_MIRROR_DIR).inheritIO().start().waitFor() != 0)
+            exitProcess(1)
+
+        commit.writeText(latestCommit)
+        
+        println("Building the bot...")
+        ProcessBuilder("./gradlew", "build").inheritIO().directory(File(REPO_MIRROR_DIR+"Core/")).start().waitFor()
+        Files.move(Paths.get(REPO_MIRROR_DIR+"Core/build/libs/Core-1.0-all.jar"), Paths.get(INSTALLATION_DIR))
+    }
 }
+
+fun getLatestCommit(): String {
+    val (_, response, result) = COMMIT_URL.httpGet().responseString()
+    return result.fold({
+        return@fold Gson().fromJson(it, CondensedCommitObj::class.java).sha
+    },{
+        exitProcess(1)
+    })
+}
+
+data class CondensedCommitObj(var sha: String)
